3440edc930ce6ecf2465bda090aa611cbc941096
Adityagaurkar09 <adityagaurkar09@gmail.com>
2025-04-27 20:34:20 +0530

    server setup completed

---
+++ server/index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import cors from 'cors';
+import nodemon from 'nodemon';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    if (conn) {
+        console.log("MongoDB Connected");
+    }
+    else {
+        console.log("MongoDB Connection Failed");
+    }
+
+};
+
+app.get('/health',(req,res)=>{
+    res.json({
+        status: "true",
+        Message:"server is running"
+    })
+})
+
+
+
+const Port = process.env.Port || 5002;
+
+app.listen(Port,()=>{
+    console.log(`Server is running on port ${Port}`);
+    connectDB();
+    console.log("MongoDB Connected");
+})
+
+
+
+
